5b589e61a6626c5e44867cfdb0ea8631a1514bd6
ankeshkr <32651031+ankeshkr@users.noreply.github.com>
2024-09-08 14:24:42 +0530

    Update online_module.py

--- Code_basics/online_basics/online_module.py
+++ Code_basics/online_basics/online_module.py
@@ -50,56 +50,56 @@ def generate_image_openai(client, prompt, model="dall-e-3", size="1024x1024", n=
 # OpenAI Text Generation
 ############################################
  
-#def generate_text_openai_streamlit(client, prompt,text_area_placeholder=None,
-#                                   model="gpt-3.5-turbo", temperature=0.5,
-#                                   max_tokens=30, top_p=1, frequency_penalty=0,
-#                                   presence_penalty=0, stream=True, html=False):
 def generate_text_openai_streamlit(client, prompt,text_area_placeholder=None,
-                                  model="gpt-3.5-turbo",
-                                  max_tokens=30):
-    response = client.chat.completions.create(
-         model = "gpt-3.5-turbo",
-         response_format="text",
-         messages=[{"role":"user", "content":prompt}]
-         )
-    text_area_placeholder.text_area("Generated Text", value=response.choices[0].message.content)
+                                  model="gpt-3.5-turbo", temperature=0.5,
+                                  max_tokens=30, top_p=1, frequency_penalty=0,
+                                  presence_penalty=0, stream=True, html=False):
+# def generate_text_openai_streamlit(client, prompt,text_area_placeholder=None,
+#                                   model="gpt-3.5-turbo",
+#                                   max_tokens=30):
+#     response = client.chat.completions.create(
+#          model = "gpt-3.5-turbo",
+#          response_format="text",
+#          messages=[{"role":"user", "content":prompt}]
+#          )
+#     text_area_placeholder.text_area("Generated Text", value=response.choices[0].message.content)
    # return response
-    # response = client.chat.completions.create(
-    #     model=model,
-    #     messages=[{"role": "user", "content": prompt}],
-    #     temperature=temperature,
-    #     max_tokens=max_tokens,
-    #     top_p=top_p,
-    #     frequency_penalty=frequency_penalty,
-    #     presence_penalty=presence_penalty,
-    #     stream=stream
-    # )
-    # complete_response = []
-    # for chunk in response:
-    #     # Ensure that chunk content is not None before appending
-    #     if chunk.choices[0].delta.content:
-    #         complete_response.append(chunk.choices[0].delta.content)
-    #         result_string = ''.join(complete_response)  # Join without additional spaces
+    response = client.chat.completions.create(
+        model=model,
+        messages=[{"role": "user", "content": prompt}],
+        temperature=temperature,
+        max_tokens=max_tokens,
+        top_p=top_p,
+        frequency_penalty=frequency_penalty,
+        presence_penalty=presence_penalty,
+        stream=stream
+    )
+    complete_response = []
+    for chunk in response:
+        # Ensure that chunk content is not None before appending
+        if chunk.choices[0].delta.content:
+            complete_response.append(chunk.choices[0].delta.content)
+            result_string = ''.join(complete_response)  # Join without additional spaces
  
-    #         # Auto Scroll
-    #         lines = result_string.count('\n') + 1
-    #         avg_chars_per_line = 80  # Adjust based on expected average line length
-    #         lines += len(result_string) // avg_chars_per_line
-    #         height_per_line = 20  # Adjust as needed
-    #         total_height = lines * height_per_line
+            # Auto Scroll
+            lines = result_string.count('\n') + 1
+            avg_chars_per_line = 80  # Adjust based on expected average line length
+            lines += len(result_string) // avg_chars_per_line
+            height_per_line = 20  # Adjust as needed
+            total_height = lines * height_per_line
  
  
-    #         if text_area_placeholder:
-    #             if html:
-    #                 text_area_placeholder.markdown(result_string, unsafe_allow_html=True)
-    #             else:
-    #                 text_area_placeholder.text_area("Generated Text", value=result_string,height=total_height)
+            if text_area_placeholder:
+                if html:
+                    text_area_placeholder.markdown(result_string, unsafe_allow_html=True)
+                else:
+                    text_area_placeholder.text_area("Generated Text", value=result_string,height=total_height)
  
-    # result_string = ''.join(complete_response)
-    # words = len(result_string.split())  # This is an approximation
-    # st.text(f"Total Words Generated: {words}")
+    result_string = ''.join(complete_response)
+    words = len(result_string.split())  # This is an approximation
+    st.text(f"Total Words Generated: {words}")
  
-    # return result_string
+    return result_string
 
 def main():
     apikey = st.secrets["OPENAI_API_KEY"]
